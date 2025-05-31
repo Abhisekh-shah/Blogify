1cd807be9a423b055607b02a4f6d2134f56cd2c4
Abhisekh_shah <abhisekhshah9@gmail.com>
2025-05-31 21:50:04 +0545

    Blogify

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express();
+const PORT =8000;
+const path= require("path");
+const userRoute= require ("./routes/user")
+const mongoose =require("mongoose")
+
+mongoose.connect("mongodb://127.0.0.1:27017/blogify").then((e)=>console.log("Mongodb connected"))
+app.set("view engine", "ejs");
+app.set("views",path.resolve("./views"))
+app.use(express.urlencoded({extended:false}))
+app.get("/",(req,res)=>{
+    res.render("home")
+})
+app.use("/user",userRoute)
+app.listen(PORT,()=> console.log(`Server started at PORT :${PORT}`));
