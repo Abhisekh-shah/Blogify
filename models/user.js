a7942cbe0f690d9a79f38c083fc6701428c9556c
Abhisekh_shah <abhisekhshah9@gmail.com>
2025-06-01 23:05:12 +0545

    Add Services floder

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const {Schema,model} =require ("mongoose");
 const { randomBytes ,createHmac} = require("crypto");
-
+const {createTokenForUser} =require ("../services/authentication")
 const userSchema =new Schema({
     fullName:{
         type:String,
@@ -43,7 +43,7 @@ userSchema.pre("save",function (next) {
    this.password=hashedPassword;
    next();
 })
-userSchema.static("matchPassword" , async function(email, password) {
+userSchema.static("matchPasswordAndGenerateToken" , async function(email, password) {
     const user = await this.findOne({ email });
     if (!user) throw new Error("User not found!");
 
@@ -56,8 +56,8 @@ userSchema.static("matchPassword" , async function(email, password) {
     if (hashedPassword !== userProvidedHash) throw new Error("Incorrect password");
         
    
-
-    return {...user , password:undefined,salt:undefined};
+  const token = createTokenForUser(user);
+  return token;
 });
 
 
